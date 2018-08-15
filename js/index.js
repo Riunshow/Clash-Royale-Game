7b3d83da33ae643926e1639f327bf90f2483f602
Riunshow <zhubotaigg@gmail.com>
2018-08-15 11:10:13 +0800

    增加切换四人动画

--- js/index.js
+++ js/index.js
@@ -224,7 +224,7 @@ function onAssetsLoaded(loader, res) {
         switch (type) {
             case 1:
                 let clickScore1_1 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore1_1.position.set((app.screen.width - img_1_width) * 0.5, (app.screen.height - kingCage1_1.height) * 0.45 - 20);
+                clickScore1_1.position.set((app.screen.width - img_1_width) * 0.5, (app.screen.height - basicCageArray[0][0].height) * 0.45 - 20);
                 app.stage.addChild(clickScore1_1);
                 var coords = {
                     x: 0,
@@ -246,9 +246,9 @@ function onAssetsLoaded(loader, res) {
                 break;
             case 2:
                 let clickScore2_1 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore2_1.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - kingCage2_1.height) * 0.5 - 30);
+                clickScore2_1.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - basicCageArray[0][1].height) * 0.5 - 30);
                 let clickScore2_2 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore2_2.position.set((app.screen.width - img_1_width) * 0.75, (app.screen.height - kingCage2_2.height) * 0.5 - 30);
+                clickScore2_2.position.set((app.screen.width - img_1_width) * 0.75, (app.screen.height - basicCageArray[0][2].height) * 0.5 - 30);
                 app.stage.addChild(clickScore2_1, clickScore2_2);
                 var coords = {
                     x: 0,
@@ -271,13 +271,13 @@ function onAssetsLoaded(loader, res) {
                 break;
             case 4:
                 let clickScore4_1 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore4_1.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - kingCage4_1.height) * 0.65 - 10);
+                clickScore4_1.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - basicCageArray[0][3].height) * 0.65 - 10);
                 let clickScore4_2 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore4_2.position.set((app.screen.width - img_1_width) * 0.8, (app.screen.height - kingCage4_2.height) * 0.65 - 10);
+                clickScore4_2.position.set((app.screen.width - img_1_width) * 0.8, (app.screen.height - basicCageArray[0][4].height) * 0.65 - 10);
                 let clickScore4_3 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore4_3.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - kingCage4_3.height) * 0.20 - 10);
+                clickScore4_3.position.set((app.screen.width - img_1_width) * 0.3, (app.screen.height - basicCageArray[0][5].height) * 0.20 - 10);
                 let clickScore4_4 = PIXI.Sprite.fromImage('./img/1.png');
-                clickScore4_4.position.set((app.screen.width - img_1_width) * 0.8, (app.screen.height - kingCage4_4.height) * 0.20 - 10);
+                clickScore4_4.position.set((app.screen.width - img_1_width) * 0.8, (app.screen.height - basicCageArray[0][6].height) * 0.20 - 10);
                 app.stage.addChild(clickScore4_1, clickScore4_2, clickScore4_3, clickScore4_4);
                 var coords = {
                     x: 0,
@@ -303,83 +303,64 @@ function onAssetsLoaded(loader, res) {
         }
     }
 
-    // king
-    let king1_1 = initCreate('king', res);
-    let king2_1 = initCreate('king', res);
-    let king2_2 = initCreate('king', res);
-    let king4_1 = initCreate('king', res)
-    let king4_2 = initCreate('king', res)
-    let king4_3 = initCreate('king', res)
-    let king4_4 = initCreate('king', res)
-
-
-    let kingCage1_1 = initCage(king1_1, 0.5, 0.5, 0.5);
-    let kingCage2_1 = initCage(king2_1, 0.3, 0.15, 0.5);
-    let kingCage2_2 = initCage(king2_2, 0.3, 0.85, 0.5);
-    let kingCage4_1 = initCage(king4_1, 0.2, 0.2, 0.25);
-    let kingCage4_2 = initCage(king4_2, 0.2, 0.8, 0.25);
-    let kingCage4_3 = initCage(king4_3, 0.2, 0.2, 0.7);
-    let kingCage4_4 = initCage(king4_4, 0.2, 0.8, 0.7);
-
-
-    // Barbarian
-    let Barbarian1_1 = initCreate('Barbarian', res);
-    let Barbarian2_1 = initCreate('Barbarian', res);
-    let Barbarian2_2 = initCreate('Barbarian', res);
-
-    let BarbarianCage1_1 = initCage(Barbarian1_1, 0.5, 0.5, 0.5);
-    let BarbarianCage2_1 = initCage(Barbarian2_1, 0.3, 0.15, 0.5);
-    let BarbarianCage2_2 = initCage(Barbarian2_2, 0.3, 0.85, 0.5);
-
-    // Goblin
-    let Goblin1_1 = initCreate('Goblin', res);
-    let Goblin2_1 = initCreate('Goblin', res);
-    let Goblin2_2 = initCreate('Goblin', res);
-
-    let GoblinCage1_1 = initCage(Goblin1_1, 0.5, 0.5, 0.5);
-    let GoblinCage2_2 = initCage(Goblin2_2, 0.3, 0.85, 0.5);
-    let GoblinCage2_1 = initCage(Goblin2_1, 0.3, 0.15, 0.5);
-
-    // Hog_Rider
-    let Hog_Rider1_1 = initCreate('Hog_Rider', res);
-    let Hog_Rider2_1 = initCreate('Hog_Rider', res);
-    let Hog_Rider2_2 = initCreate('Hog_Rider', res);
-
-    let Hog_RiderCage1_1 = initCage(Hog_Rider1_1, 0.5, 0.5, 0.5);
-    let Hog_RiderCage2_1 = initCage(Hog_Rider2_1, 0.3, 0.85, 0.5);
-    let Hog_RiderCage2_2 = initCage(Hog_Rider2_2, 0.3, 0.15, 0.5);
-
-
-
     let tmpIndex = 1
-    let tmpArray = [kingCage1_1, BarbarianCage1_1, GoblinCage1_1, Hog_RiderCage1_1]
-    let testTmpArray = [
-        [
-            kingCage1_1,
-            kingCage2_1,
-            kingCage2_2,
-            kingCage4_1,
-            kingCage4_2,
-            kingCage4_3,
-            kingCage4_4,
-        ],
-        [
-            BarbarianCage1_1,
-            BarbarianCage2_1,
-            BarbarianCage2_2,
-        ],
-        [
-            GoblinCage1_1,
-            GoblinCage2_2,
-            GoblinCage2_1,
-        ],
-        [
-            Hog_RiderCage1_1,
-            Hog_RiderCage2_1,
-            Hog_RiderCage2_2,
-        ]
-    ]
-    app.stage.addChild(testTmpArray[0][0]);
+
+    let basicSpineArray = []
+    let king = [],
+        Barbarian = [],
+        Goblin = [],
+        Hog_Rider = [];
+    for (let i = 0; i < 7; i++) {
+        king.push(initCreate('king', res));
+        Barbarian.push(initCreate('Barbarian', res));
+        Goblin.push(initCreate('Goblin', res));
+        Hog_Rider.push(initCreate('Hog_Rider', res));
+    }
+
+    basicSpineArray = [king, Barbarian, Goblin, Hog_Rider];
+
+    let basicCageArray = [
+        [],
+        [],
+        [],
+        []
+    ];
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 7; j++) {
+            switch (j) {
+                case 0:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.5, 0.5, 0.5);
+                    break;
+                case 1:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.3, 0.15, 0.5);
+                    break;
+                case 2:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.3, 0.85, 0.5)
+                    break;
+                case 3:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.2, 0.2, 0.25)
+                    break;
+                case 4:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.2, 0.8, 0.25)
+                    break;
+                case 5:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.2, 0.2, 0.7)
+                    break;
+                case 6:
+                    basicCageArray[i][j] = initCage(basicSpineArray[i][j], 0.2, 0.8, 0.7)
+                    break;
+                default:
+                    break;
+            }
+            basicSpineArray[i][j].state.timeScale = 1;
+            basicSpineArray[i][j].state.setAnimation(0, 'Shaking', true, 0);
+        }
+
+    }
+
+
+    app.stage.addChild(basicCageArray[0][0]);
 
 
     function changeAnimation() {
@@ -387,35 +368,28 @@ function onAssetsLoaded(loader, res) {
         if (loadprogress) {
             app.stage.swapChildren(background2, background1)
 
-            // if (tmpIndex >= tmpArray.length) {
-            //     app.stage.removeChild(tmpArray[tmpArray.length - 1]);
-
-            //     tmpIndex = 0;
-            // } else {
-            //     app.stage.removeChild(tmpArray[tmpIndex - 1]);
-            // }
-            // app.stage.addChild(tmpArray[tmpIndex]);
-
             console.log(`tmpIndex: ${tmpIndex}`)
 
-            if (tmpIndex >= testTmpArray.length) {
-                for (const key in testTmpArray[testTmpArray.length - 1]) {
-                    app.stage.removeChild(testTmpArray[testTmpArray.length - 1][key]);
+            if (tmpIndex >= basicCageArray.length) {
+                for (const key in basicCageArray[basicCageArray.length - 1]) {
+                    app.stage.removeChild(basicCageArray[basicCageArray.length - 1][key]);
                 }
 
                 tmpIndex = 0;
             } else {
-                for (const key in testTmpArray[tmpIndex - 1]) {
-                    app.stage.removeChild(testTmpArray[tmpIndex - 1][key]);
+                for (const key in basicCageArray[tmpIndex - 1]) {
+                    app.stage.removeChild(basicCageArray[tmpIndex - 1][key]);
                 }
             }
 
             if (data < 8) {
-                // app.stage.removeChild(testTmpArray[tmpIndex][1], testTmpArray[tmpIndex][2]);
-                app.stage.addChild(testTmpArray[tmpIndex][0]);
+                // app.stage.removeChild(basicCageArray[tmpIndex][1], basicCageArray[tmpIndex][2]);
+                app.stage.addChild(basicCageArray[tmpIndex][0]);
             } else if (data >= 8 && data < 16) {
-                // app.stage.removeChild(testTmpArray[tmpIndex][0]);
-                app.stage.addChild(testTmpArray[tmpIndex][1], testTmpArray[tmpIndex][2]);
+                // app.stage.removeChild(basicCageArray[tmpIndex][0]);
+                app.stage.addChild(basicCageArray[tmpIndex][1], basicCageArray[tmpIndex][2]);
+            } else {
+                app.stage.addChild(basicCageArray[tmpIndex][3], basicCageArray[tmpIndex][4], basicCageArray[tmpIndex][5], basicCageArray[tmpIndex][6]);
             }
 
 
@@ -431,47 +405,11 @@ function onAssetsLoaded(loader, res) {
         changeAnimation()
     }, "3000");
 
-    Barbarian1_1.state.timeScale = 1
-    Barbarian2_1.state.timeScale = 1
-    Barbarian2_2.state.timeScale = 1
-
-    Barbarian1_1.state.setAnimation(0, 'Shaking', true, 0);
-    Barbarian2_1.state.setAnimation(0, 'Shaking', true, 0);
-    Barbarian2_2.state.setAnimation(0, 'Shaking', true, 0);
-
-    Hog_Rider1_1.state.timeScale = 1
-    Hog_Rider2_1.state.timeScale = 1
-    Hog_Rider2_2.state.timeScale = 1
-
-    Hog_Rider1_1.state.setAnimation(0, 'Shaking', true, 0);
-    Hog_Rider2_1.state.setAnimation(0, 'Shaking', true, 0);
-    Hog_Rider2_2.state.setAnimation(0, 'Shaking', true, 0);
-
-    Goblin1_1.state.timeScale = 1
-    Goblin2_1.state.timeScale = 1
-    Goblin2_2.state.timeScale = 1
-
-    Goblin1_1.state.setAnimation(0, 'Shaking', true, 0);
-    Goblin2_1.state.setAnimation(0, 'Shaking', true, 0);
-    Goblin2_2.state.setAnimation(0, 'Shaking', true, 0);
-
-    king1_1.state.timeScale = 1
-    king1_1.state.setAnimation(0, 'Shaking', true, 0);
-    // king1_1.stateData.setMix('Idle', 'Shaking', 2);
-    // king1_1.stateData.setMix('Shaking', 'Idle', 2);
-    king2_1.state.setAnimation(0, 'Shaking', true, 0);
-    king2_2.state.setAnimation(0, 'Shaking', true, 0);
-
-    king4_1.state.setAnimation(0, 'Shaking', true, 0);
-    king4_2.state.setAnimation(0, 'Shaking', true, 0);
-    king4_3.state.setAnimation(0, 'Shaking', true, 0);
-    king4_4.state.setAnimation(0, 'Shaking', true, 0);
-
     function show() {
         console.log(data);
         switch (data) {
             case 0:
-                king1_1.state.timeScale = 0.2
+                // king1_1.state.timeScale = 0.2
                 break;
 
             case 1:
@@ -481,66 +419,70 @@ function onAssetsLoaded(loader, res) {
                 // if (file == 2) {
                 //     king1_1.state.setAnimation(0, 'Shaking', true)
                 // }
-                king1_1.state.timeScale = 1.4
+                // king1_1.state.timeScale = 1.4
                 break;
 
             case 2:
-                king1_1.state.timeScale = 1.6
+                // king1_1.state.timeScale = 1.6
                 break;
             case 7:
                 if (file == 0) {
                     // app.stage.removeChild(kingCage2_1, kingCage2_2);
                     // app.stage.addChild(kingCage1_1)
-                    app.stage.removeChild(testTmpArray[tmpIndex - 1][1], testTmpArray[tmpIndex - 1][2]);
-                    app.stage.addChild(testTmpArray[tmpIndex - 1][0]);
+                    app.stage.removeChild(basicCageArray[tmpIndex - 1][1], basicCageArray[tmpIndex - 1][2]);
+                    app.stage.addChild(basicCageArray[tmpIndex - 1][0]);
                 }
-                king1_1.state.timeScale = 1.8;
-                king2_1.state.timeScale = 2.6;
-                king2_2.state.timeScale = 2.6;
+                // king1_1.state.timeScale = 1.8;
+                // king2_1.state.timeScale = 2.6;
+                // king2_2.state.timeScale = 2.6;
                 break;
             case 8:
                 if (file == 2) {
                     // app.stage.removeChild(kingCage1_1, kingCage4_1, kingCage4_2, kingCage4_3, kingCage4_4);
                     // app.stage.addChild(kingCage2_1, kingCage2_2)
-                    app.stage.removeChild(testTmpArray[tmpIndex - 1][0]);
-                    app.stage.addChild(testTmpArray[tmpIndex - 1][1], testTmpArray[tmpIndex - 1][2]);
+                    app.stage.removeChild(basicCageArray[tmpIndex - 1][0]);
+                    app.stage.addChild(basicCageArray[tmpIndex - 1][1], basicCageArray[tmpIndex - 1][2]);
                 }
-                king1_1.state.timeScale = 2.0;
-                king2_1.state.timeScale = 2.6;
-                king2_2.state.timeScale = 2.6;
+                // king1_1.state.timeScale = 2.0;
+                // king2_1.state.timeScale = 2.6;
+                // king2_2.state.timeScale = 2.6;
                 break;
             case 10:
-                king1_1.state.timeScale = 2.8
+                // king1_1.state.timeScale = 2.8
                 break;
             case 15:
                 if (file == 0) {
                     // app.stage.removeChild(kingCage1_1, kingCage4_1, kingCage4_2, kingCage4_3, kingCage4_4);
                     // app.stage.addChild(kingCage2_1, kingCage2_2)
+                    app.stage.removeChild(basicCageArray[tmpIndex - 1][0], basicCageArray[tmpIndex - 1][3], basicCageArray[tmpIndex - 1][4], basicCageArray[tmpIndex - 1][5], basicCageArray[tmpIndex - 1][6]);
+                    app.stage.addChild(basicCageArray[tmpIndex - 1][1], basicCageArray[tmpIndex - 1][2])
                 }
-                king2_1.state.timeScale = 2.6;
-                king2_2.state.timeScale = 2.6;
-                king4_1.state.timeScale = 4;
-                king4_2.state.timeScale = 4;
-                king4_3.state.timeScale = 4;
-                king4_4.state.timeScale = 4;
+                // king2_1.state.timeScale = 2.6;
+                // king2_2.state.timeScale = 2.6;
+                // king4_1.state.timeScale = 4;
+                // king4_2.state.timeScale = 4;
+                // king4_3.state.timeScale = 4;
+                // king4_4.state.timeScale = 4;
                 break;
             case 16:
-                king2_1.state.timeScale = 2.8;
-                king2_2.state.timeScale = 2.8;
+                // king2_1.state.timeScale = 2.8;
+                // king2_2.state.timeScale = 2.8;
                 if (file == 2) {
                     // app.stage.removeChild(kingCage2_1, kingCage2_2);
                     // app.stage.addChild(kingCage4_1, kingCage4_2, kingCage4_3, kingCage4_4)
+                    app.stage.removeChild(basicCageArray[tmpIndex - 1][1], basicCageArray[tmpIndex - 1][2]);
+                    app.stage.addChild(basicCageArray[tmpIndex - 1][3], basicCageArray[tmpIndex - 1][4], basicCageArray[tmpIndex - 1][5], basicCageArray[tmpIndex - 1][6])
                 }
                 break;
             case 18:
-                king2_1.state.timeScale = 4.2;
-                king2_2.state.timeScale = 4.2;
+                // king2_1.state.timeScale = 4.2;
+                // king2_2.state.timeScale = 4.2;
                 break;
             case 20:
-                king4_1.state.timeScale = 6;
-                king4_2.state.timeScale = 6;
-                king4_3.state.timeScale = 6;
-                king4_4.state.timeScale = 6;
+                // king4_1.state.timeScale = 6;
+                // king4_2.state.timeScale = 6;
+                // king4_3.state.timeScale = 6;
+                // king4_4.state.timeScale = 6;
                 break;
         }
     }
